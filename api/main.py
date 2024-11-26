7bbf13284da807e1b84b4dd0b65491a5580d0487
an920107 <an920107@gmail.com>
2024-11-26 23:10:54 +0800

    API-9 feat: fake return data

--- api/main.py
+++ api/main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+import asyncio
+
+from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.responses import PlainTextResponse, StreamingResponse
 
 app = FastAPI()
 
-@app.get("/", status_code=204)
-def root():
-    return None
+
+@app.get("/")
+def root(request: Request) -> PlainTextResponse:
+    return f"Please refer to {request.base_url}docs or {request.base_url}redoc for the API document."
+
+
+@app.post("/message/{course_id}/send")
+async def message_send(course_id: str, request: Request) -> StreamingResponse:
+    cookie = request.headers.get("X-With-Cookie")
+    if cookie is None:
+        return HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Missing necessary headers",
+        )
+
+    async def response():
+        for c in f"Hello, {course_id}":
+            await asyncio.sleep(0.25)
+            yield f"{c}\r\n"
+
+    return StreamingResponse(response())
